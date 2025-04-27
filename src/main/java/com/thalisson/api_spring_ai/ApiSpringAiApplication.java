1cf9dd4bec7d4f3578d3a08028e9936eb1c2a6c0
Thalisson <thalissonsousa33@gmail.com>
2025-04-27 18:42:37 -0300

    Initial commit

---
+++ src/main/java/com/thalisson/api_spring_ai/ApiSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.thalisson.api_spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringAiApplication.class, args);
+	}
+
+}
